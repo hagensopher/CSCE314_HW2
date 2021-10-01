e1b6ee790e458345c9544a964eb5a086adb31bea
Hagen <hagen.sopher@tamu.edu>
2021-10-01 10:45:53 -0500

    Started Q4

---
+++ Main4.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.*;
+
+public class Main4 {
+
+    public static void main(String[] args){
+        MyClass myClass = new MyClass();
+        Class c = myClass.getClass(); //needs to be a command line argument but testing with hard case
+
+        Method[] methods = c.getDeclaredMethods();
+        for(int i =0;i<methods.length;i++){
+            String methodName = methods[i].getName();
+            if((methodName.startsWith("test")) && (methods[i].getReturnType().equals(Boolean.TYPE)) && (methods[i].getParameterCount() ==0)){
+                System.out.println("Found the method "+ methodName);
+            }
+        }
+
+    }
+
+
+
+
+}
+class MyClass{ //example temp class
+    public boolean test1(){ return true;} //correct
+    public boolean test2(){ return false;} //correct
+    public boolean testTAMU(boolean tamu){return tamu;}
+    public int testInt(){return 314;}
+    public boolean badtest(){return true;}
+}
