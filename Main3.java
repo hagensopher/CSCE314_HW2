171a8ccf1db1b2512d5e67e2c5e1b30f04b19128
Hagen <hagen-sopher@tamu.edu>
2021-10-06 18:02:48 -0500

    formatting q3

--- Main3.java
+++ Main3.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Main3{
 
     public static void main(String[] args){
-        A class1 = new A();
-        A.displayMethodInfo(class1);
+        A class1 = new A(); //create the object
+        A.displayMethodInfo(class1); //test the method code
     }
 
 }
-class A{
+class A{ //Temp Class for testing
     int a =0;
     int b=0;
     double c =0.0;
@@ -31,10 +31,16 @@ class A{
             //output methodName ( Param1, Param2) -> retrun type
             Parameter[] paramList = listMethods[i].getParameters();
             System.out.print(listMethods[i].getName()+" ("); 
-            for(int j=0;j<paramList.length;j++){
-                System.out.print(paramList[j].getType()+", "); 
+            if(paramList.length == 0){
+                System.out.println(") -> " + listMethods[i].getReturnType()); //get return type
             }
-            System.out.println(") -> " + listMethods[i].getReturnType());
+            else{
+                System.out.print(paramList[0].getType()); //grabs the first one for formatting
+                for(int j=1;j<paramList.length;j++){
+                    System.out.print(" , "+paramList[j].getType());  //diaply the rest
+                }
+                System.out.println(") -> " + listMethods[i].getReturnType()); //get return type
+            }
         }
     }
 }
